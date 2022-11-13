6ce1ef56a5b9c18cfd965d224ab40f139eb12c0a
harihandika <harihandika456@gmail.com>
2022-11-13 21:34:19 +0700

    javascript

--- functionBelajar.js
+++ functionBelajar.js
@@ -35,10 +35,36 @@
 // }
 // console.log("ini function", volumeKubus(10,3))
 
-function tambah(a,b) {
-    return a + b;
+// function tambah(a,b) {
+//     return a + b;
+// }
+
+// function kali(a ,b) {
+//      return a * b
+// }
+// let a =  tambah(1,2)
+// let b = tambah(3,4)
+// let hasil =  kali(a, b);
+// let a = parseInt(prompt('Masukkan nilai 1: '));
+// let b = parseInt(prompt('Masukkan nilai 2: '));
+// let hasil = tambah(a+10,b+10)
+// console.log(a,b);
+// console.log(hasil);
+
+//  function  tambah() {
+// return arguments;
+//  }
+
+// let coba = tambah(5,10,20,"hi",false);
+// console.log(coba);
+
+function  tambah() {
+    let hasil = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        hasil += arguments[i];
+    }
+    return hasil;
 }
-let a = parseInt(prompt('Masukkan nilai 1: '));
-let b = parseInt(prompt('Masukkan nilai 2: '));
-let hasil = tambah(a+10,b+10)
-console.log(hasil);
+
+let coba = tambah(1,2,3,4,5);
+console.log(coba);
